58737522390c7073a7f707e7c479957dfacab9b3
sotirelisc <gtakriz@hotmail.com>
2019-03-15 12:04:23 +0200

    Fix env vars & add basic todo actions.

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import * as firebase from 'firebase';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config({ path: '../.env' });
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 }
+
+console.log(config);
 
 firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
 
-export const todosRef = databaseRef.child("todos");
+export const todosRef = databaseRef.child("toreact");
