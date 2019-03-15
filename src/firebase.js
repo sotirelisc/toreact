0589d0e257a1769f5e8a52697b81e16f29dd7991
sotirelisc <gtakriz@hotmail.com>
2019-03-15 11:34:40 +0200

    Add Firebase config & create basic data reducer.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+require('dotenv').config();
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+
+export const todosRef = databaseRef.child("todos");
